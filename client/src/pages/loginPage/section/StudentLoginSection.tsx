642fd8188da59878db30a327eda586db25977f2b
Mathimalar190505 <malarmathi826@gmail.com>
2025-08-24 15:41:39 +0530

    login pages

---
+++ client/src/pages/loginPage/section/StudentLoginSection.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const StudentLoginSection: React.FC = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <div className="flex flex-col md:flex-row bg-white justify-center rounded-2xl shadow-lg w-full max-w-4xl border-2 border-[#8b5e3c] mb-3">
+      <div className="mt-3 w-full px-6">
+        <p className="text-black text-center font-bold text-3xl mb-6">
+          Student Login
+        </p>
+
+        {/* Register Number */}
+        <input
+          type="number"
+          placeholder="Register Number"
+          className="block mx-auto w-[90%] max-w-md p-2 rounded mb-4 bg-white border border-[#8b5e3c] 
+                     text-[#5a3e2b] placeholder-black 
+                     focus:outline-none focus:ring-2 focus:ring-[#b77039]
+                     appearance-none 
+                     [&::-webkit-outer-spin-button]:appearance-none 
+                     [&::-webkit-inner-spin-button]:appearance-none"
+        />
+
+        {/* Password Field + Eye */}
+        <div className="relative mb-1 w-[90%] max-w-md mx-auto">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Enter your password"
+            className="w-full p-2 rounded bg-white border border-[#8b5e2c] 
+                       text-[#5a3e2b] placeholder-black 
+                       focus:outline-none focus:ring-2 focus:ring-[#b77039]"
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-2 top-3 text-black]"
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        </div>
+
+        {/* Forgot Password */}
+        <div className="flex justify-end w-[90%] max-w-md mx-auto mb-4">
+          <a href="#" className="text-sm text-blue-800">
+            Forgot Password?
+          </a>
+        </div>
+
+        {/* Login Button */}
+        <button className="block mx-auto w-[90%] max-w-md bg-[#8b4513] p-2 rounded mb-4 text-white hover:bg-[#b77039] transition-colors duration-300">
+          Log in
+        </button>
+
+        {/* New User Link */}
+        <p className="text-[#5a3e2b] mb-6 text-center">
+          <a
+            href="#"
+            onClick={(e) => e.preventDefault()}
+            className="text-blue-800 underline"
+          >
+            New User?
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StudentLoginSection;
