96c1e24dc9784584424ad10b098b0a6055835a64
Sailash05 <sailashs2005@gmail.com>
2025-08-24 22:11:55 +0530

    Bug fix in login loading component

--- client/src/components/loadingComponent/LoginLoading.tsx
+++ client/src/components/loadingComponent/LoginLoading.tsx
@@ -1,20 +1,23 @@
-import './LoginLoading.css'
+import './loginLoading.css'
 
 
 const LoginLoading = () => {
-  return (
-    <div className="wrapper">
-      <div className="box-wrap">
-        <div className="box one"></div>
-        <div className="box two"></div>
-        <div className="box three"></div>
-        <div className="box four"></div>
-        <div className="box five"></div>
-        <div className="box six"></div>
-      </div>
-      <div className="loading-text">Logging in...</div>
-    </div>
-  )
+    return (
+        <div className='outer-container'>
+            <div className="wrapper">
+                <div className="box-wrap">
+                    <div className="box one"></div>
+                    <div className="box two"></div>
+                    <div className="box three"></div>
+                    <div className="box four"></div>
+                    <div className="box five"></div>
+                    <div className="box six"></div>
+                </div>
+                <div className="loading-text">Logging in...</div>
+            </div>
+        </div>
+        
+    )
 }
 
 export default LoginLoading
