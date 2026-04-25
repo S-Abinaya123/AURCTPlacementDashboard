85b3f52d258e7cef712451a0558240e9105c6435
934205 <nadesanjayarani4@gmail.com>
2026-04-25 18:09:20 +0530

    latest

--- client/src/api/roadmapApi.ts
+++ client/src/api/roadmapApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const testApi = async () => {
   const response = await axios.get(`${API_URL}/roadmap/test`);
