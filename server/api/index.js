1d3289c0d675a5c6d6360e1809d774c9c9b0be2a
934205 <nadesanjayarani4@gmail.com>
2026-04-27 21:29:30 +0530

    latest

--- server/api/index.js
+++ server/api/index.js
@@ -18,6 +18,18 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 /* ================= CORS ================= */
 app.use(
   cors({
@@ -29,6 +41,7 @@ app.use(
     credentials: true,
   })
 );
+app.options("*", cors());
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
